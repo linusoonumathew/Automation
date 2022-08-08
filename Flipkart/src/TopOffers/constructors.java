01f676cd0e7bfb88467d4955165ce93dd7540c3c
LINU <LINU@OL-38>
2022-08-08 20:29:55 +0530

    Automation

---
+++ Flipkart/src/TopOffers/constructors.java
@@ -0,0 +1,10 @@
+package TopOffers;
+
+public class constructors {
+	public static void main(String[] args) { 
+		constructors cp = new constructors();
+	} 
+constructors(){
+	System.out.println("Linu");
+}
+}
